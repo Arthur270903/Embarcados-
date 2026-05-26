cf33e21b460db57b67329903e0a66e3544c66f75
Arthur Santos Alencar <arthur.alencar@usp.br>
2026-05-26 10:35:00 -0300

    LED com registrador

--- aula5/Aula5_piscaLedRegistrador/src/main.c
+++ aula5/Aula5_piscaLedRegistrador/src/main.c
@@ -1,18 +1,16 @@
 /* Definir Endereço dos Registradores */
 #define SIM_SCGC5      (*(volatile unsigned int*)0x40048038)
-#define PORTB_PCR19    (*(volatile unsigned int*)0x4004A04C) // Configuração do Pino 19 (Porta B)
-#define GPIOB_PDDR     (*(volatile unsigned int*)0x400FF054) // Direção dos Pinos da Porta B
-#define GPIOB_PSOR     (*(volatile unsigned int*)0x400FF044) // Set (Coloca o pino em 1 / Apaga o LED)
-#define GPIOB_PCOR     (*(volatile unsigned int*)0x400FF048) // Clear (Coloca o pino em 0 / Acende o LED)
-
+#define PORTB_PCR19    (*(volatile unsigned int*)0x4004A04C)
+#define GPIOB_PDDR     (*(volatile unsigned int*)0x400FF054) 
+#define GPIOB_PSOR     (*(volatile unsigned int*)0x400FF044) 
+#define GPIOB_PCOR     (*(volatile unsigned int*)0x400FF048) 
 void delayMs(int n); 
-/* Função: Espera n milisegundos CORRIGIDA */
 void delayMs(int n) {
-    volatile int i; // <--- Adicionado 'volatile'
-    volatile int j; // <--- Adicionado 'volatile'
+    volatile int i; 
+    volatile int j; 
     for (i = 0; i < n; i++) {
         for (j = 0; j < 7000; j++) {
-            // O compilador agora é obrigado a executar este bloco linha por linha
+            
         }
     }
 }
@@ -26,20 +24,19 @@ int main(void) {
     GPIOB_PDDR |= (1 << 19); 
 
   
-    for (; ; ) { // 
+    for (; ; ) { 
         
    
-        GPIOB_PCOR = (1 << 19); // [cite: 8]
+        GPIOB_PCOR = (1 << 19); 
 
-        /* (5) Função de espera (1000 milisegundos = 1 segundo) */
-        delayMs(1000); // [cite: 9]
+        
+        delayMs(1000); 
 
-        /* (6) Desabilitar saída (Desliga o LED verde) */
-        // Colocamos o pino em 1 (Set) para cortar a corrente e apagar o LED
-        GPIOB_PSOR = (31<< 19); // [cite: 10]
+      
+        GPIOB_PSOR = (1<< 19); 
 
     
-        delayMs(1000); // [cite: 11]
+        delayMs(1000);
     }
 
     return 0;
